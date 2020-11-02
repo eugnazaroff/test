0ffacf1ab9f278258636ca5ed44c645ed8ca1823
eugene <eugnazaroff@gmail.com>
2020-11-02 23:43:33 +0400

    adding files for gh pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from "react";
+import {CardList} from './components/card-list/card-list'
+import {SearchBox} from './components/search-box/search-box'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        //Init state
+        this.state = {
+            monsters: [],
+            searchField: ""
+        }
+    }
+
+    /*
+    Fetching data from fake API in the lifecycle method componentDidMount
+    Notice that this method is async so we can use await in the fetch method
+    Fetch method returns a resolved promise with a full response
+    Next we again await for .json() method that returns another promise with a body
+     */
+
+    async componentDidMount() {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await response.json()
+        this.setState({monsters: users})
+    }
+
+    render() {
+        const {monsters, searchField} = this.state
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase()))
+        return (
+            <div className='App'>
+                <h1>Monsters Rolodex</h1>
+                <SearchBox
+                    placeholder=" search me"
+                    handleChange={e =>
+                        this.setState({searchField: e.target.value})
+                    }
+                />
+                <CardList monsters={filteredMonsters}/>
+            </div>
+        )
+    }
 }
 
 export default App;
